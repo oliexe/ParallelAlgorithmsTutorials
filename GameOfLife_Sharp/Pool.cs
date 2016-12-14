82d68e7d6d8c80234adcef6fb8b3eae8c10b9f41
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2016-12-14 17:21:14 +0100

    Homework 3 - Game of Life in C#

---
+++ GameOfLife_Sharp/Pool.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    public class Pool
+    {
+        /// <summary>
+        /// Pole buněk, aktuální a další generace
+        /// </summary>
+        public bool[,] pool;
+        public bool[,] next;
+
+        /// <summary>
+        /// Velikost, Generace buněk
+        /// </summary>
+        public int Size = 0;
+        public int Generation = 0;
+
+        private Task processTask;
+        public Pool() { }
+        public bool this[int x, int y]
+        {
+            get { return this.pool[x, y]; }
+            set { this.pool[x, y] = value; }
+        }
+
+        /// <summary>
+        /// Nastavi velikost GoL pole
+        /// </summary>
+        /// <param name="size">Velikost pole (size x size)</param>
+        public void SetPool(int size)
+        {
+            this.Size = size;
+            pool = new bool[size, size];
+            next = new bool[size, size];
+        }
+
+        /// <summary>
+        /// Počítá živé sousedy buňky
+        /// </summary>
+        /// <param name="world">Vstupní pool buněk </param>
+        /// <param name="size">Velikost poolu</param>
+        /// <param name="x">X buňky</param>
+        /// <param name="y">Y buňky</param>
+        /// <returns></returns>
+        private static int CountLive(bool[,] world, int size, int x, int y)
+        {
+            //Region (offset) ve kterém hledáme
+            int[,] region = new int[,] { { -1, 0 }, { -1, 1 },
+                { 0, 1 }, { 1, 1 }, { 1,0 }, { 1, -1 }, { 0, -1 }, { -1, -1 } };
+            int result = 0;
+
+            for (int i = 0; i <= 7; i++)
+            {
+                int region_count = i;
+
+                int regionx = x + region[region_count, 0];
+                int regiony = y + region[region_count, 1];
+
+                bool range = regionx < 0 || regionx >= size
+                                   | regiony < 0 || regiony >= size;
+                if (range == false)
+                {
+                    int result2 = world[x + region[region_count, 0],
+                        y + region[region_count, 1]] ? 1 : 0;
+
+                    result = result + result2;
+                }
+            }
+            return result;
+
+        }
+
+        /// <summary>
+        /// Spustit generaci GoL
+        /// </summary>
+        public void Begin()
+        {
+            //Pouze pokud je předchozí task dokončen 
+            if (this.processTask == null)
+            {
+                this.processTask = this.NextGeneration();
+            }
+        }
+
+        /// <summary>
+        /// Přepnout buňku mezi živá/mrtvá
+        /// </summary>
+        /// <param name="x">x buňky v poli</param>
+        /// <param name="y">y buňky v poli</param>
+        /// <returns></returns>
+        public bool SwitchCell(int x, int y)
+        {
+            bool currentValue = this.pool[x, y];
+            return this.pool[x, y] = !currentValue;
+        }
+
+        /// <summary>
+        /// Updatuje pole (Přesouvá výsledky z next pole do aktuálního pole) po dokončení všech threadu
+        /// </summary>
+        public void Update()
+        {
+            if (this.processTask != null && this.processTask.IsCompleted)
+            {
+                var flip = this.next;
+                this.next = this.pool;
+                this.pool = flip;
+                Generation++;
+
+                this.processTask = this.NextGeneration();
+            }
+        }
+
+        /// <summary>
+        /// Generovat další generaci herního pole - zde jsou uplatněny GoL pravidla
+        /// </summary>
+        /// <returns></returns>
+        private Task NextGeneration()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Parallel.For(0, Size, x =>
+                {
+                    Parallel.For(0, Size, y =>
+                    {
+                        // Spočitat živé sousedy
+                        int n = CountLive(pool, Size, x, y);
+
+                //Pravidlo 1 - Ziva bunka s mene, nez dvema zivymi sousedy umira.
+                //Pravidlo 2 - Ziva bunka s vice, nez tremi zivymi sousedy umira. 
+                //Pravidlo 3 - Ziva bunka s dvoumi nebo tremi sousedy preziva beze zmeny do dalsi generace.	
+                        next[x, y] = false;
+                        if (pool[x, y] && (n == 2 || 
+                                        n == 3))
+                            next[x, y] = true;
+
+                //Pravidlo 4 - Mrtva bunka, s presne tremi zivymi sousedy, opet oziva.
+                        else if (!pool[x, y] && 
+                                  n == 3)
+                            next[x, y] = true;
+                    });
+                });
+            });
+        }
+    }
+
+}
---
+++ GameOfLife_Sharp/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    class Program
+    {
+        public static Pool _sim = new Pool();
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            ReadInput(args[0]);
+            else
+            ReadInput("sample_input.txt");
+
+            _sim.Begin();
+
+            while (true)
+            {
+                Console.Clear();
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                _sim.Update();
+                watch.Stop();
+                Print(_sim, watch.Elapsed.Ticks, _sim.Generation);
+
+                Thread.Sleep((int)TimeSpan.FromSeconds(0.3).TotalMilliseconds);
+            }
+        }
+
+        /// <summary>
+        /// Importuje textový soubor do GoL pole
+        /// </summary>
+        /// <param name="fileName">Vstupní textový soubor</param>
+        private static void ReadInput(string fileName)
+        {
+            String input = File.ReadAllText(fileName);
+
+            int i = 0, j = 0;
+            int[,] result = new int[25, 25];
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    result[i, j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+
+            int rowLength = result.GetLength(0);
+            int colLength = result.GetLength(1);
+            _sim.SetPool(result.GetLength(0));
+
+            for (int printx = 0; printx < rowLength; printx++)
+            {
+                for (int printy = 0; printy < colLength; printy++)
+                {
+                    if (result[printx, printy] == 1)
+                    _sim.SwitchCell(printx, printy);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tisk pole do konzole
+        /// </summary>
+        /// <param name="sim">Vstupní Simulace</param>
+        /// <param name="time">Čas vygenerování</param>
+        /// <param name="generation">Generace pole</param>
+        private static void Print(Pool sim, double time, int generation)
+        {
+            for (int printy = 0; printy < sim.Size; printy++)
+            {
+                for (int printx = 0; printx < sim.Size; printx++) 
+                    Console.Write(sim[printx, printy] ? " X " : " - ");
+                Console.WriteLine();
+            }
+            Console.WriteLine("Generation : " + generation);
+            Console.WriteLine("Generated in: " + time + " ticks");
+        }
+    }
+}
